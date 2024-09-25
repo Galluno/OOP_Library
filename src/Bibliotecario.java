f3319d411eeaa988b3df136926cb43bd9d55ff84
Nicola Galluccio <galluccio.nicola@gmail.com>
2024-09-25 16:29:56 +0200

    Fixed wrong parenthesis

--- src/Bibliotecario.java
+++ src/Bibliotecario.java
@@ -1,17 +1,19 @@
 public class Bibliotecario {
 
-    public Bibliotecario(
-                /*TODO: builder and all methods
-                    methods: registrazione prestiti
-                            verifica disponibilità libro
-                            registrare restituzione libro
-                            generare report su libri in prestito
-                            generare report su utenti che hanno libri in prestito
-                            verificare quali libri sono in ritardo di restituzione
+    public Bibliotecario()
+    {
+
+    }
+
+    /*TODO: builder and all methods
+            methods: registrazione prestiti
+                     verifica disponibilità libro
+                     registrare restituzione libro
+                     generare report su libri in prestito
+                     generare report su utenti che hanno libri in prestito
+                     verificare quali libri sono in ritardo di restituzione
 
 
                  */
-    )
-
 
 }
