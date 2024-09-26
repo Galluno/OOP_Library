2b24c8f0fb1224e1d8171c49e8e1c2e83e280596
Nicola Galluccio <galluccio.nicola@gmail.com>
2024-09-26 16:41:23 +0200

    Bibliotecario: added registraprestito

--- src/Bibliotecario.java
+++ src/Bibliotecario.java
@@ -1,8 +1,34 @@
+import lombok.Getter;
+
 public class Bibliotecario {
+
+    @Getter
+    String codBibliotecario;
+    private final Prestito prestito;
+    private final @Getter LibroFactory librofactory = new LibroFactory();
 
     public Bibliotecario()
     {
+        this.prestito = new Prestito();
 
+    }
+
+
+    /**
+     * Registra un prestito per l'utente specificato.
+     * Lancia un'eccezione IllegalUtenteException se l'utente è sospeso.
+     * Lancia un'eccezione IllegalLibroAmount se il libro non è disponibile.
+     *
+     * @param utente l'utente per il quale si vuole registrare il prestito
+     * @param libro  il libro che si vuole prestare
+     */
+    public void registraPrestiti(Utente utente, Libro libro) {
+        if (!utente.isSospeso()) {
+            if (!checkDisponibilità(libro)) {
+                libro.uscitaLibro(libro);
+                new Prestito(libro, utente);
+            } else throw new IllegalLibroAmount("Libro non disponibile");
+        } else throw new IllegalUtenteException("L'utente è sospeso");
     }
 
     /*TODO: builder and all methods
@@ -12,8 +38,23 @@ public class Bibliotecario {
                      generare report su libri in prestito
                      generare report su utenti che hanno libri in prestito
                      verificare quali libri sono in ritardo di restituzione
-
-
                  */
+
+
+    /**
+     * Registra la restituzione di un libro
+     *
+     * @param libro il libro da restituire
+     */
+    public void registraRestituzione(Libro libro) {
+        libro.addLibro(libro);
+    }
+
+
+    public boolean checkDisponibilità(Libro libro) {
+        return libro.getDisponibilità() > 0;
+    }
+
+
 
 }
