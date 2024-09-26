00a80462ba4715d5ee0f0b8c10d37f3d2f7064de
Nicola Galluccio <galluccio.nicola@gmail.com>
2024-09-26 16:43:36 +0200

    prestito now does something

--- src/Prestito.java
+++ src/Prestito.java
@@ -1,12 +1,44 @@
-import java.util.Date;
-import java.util.HashSet;
-import java.util.Set;
+import java.time.LocalDate;
+import java.util.Observer;
 
-public class Prestito {
+public class Prestito implements Observable {
+    public LocalDate data;
+    public long inizioPrestito;
+    private long finePrestito;
+        /*
+        PRESTITO: rappresenta tutti i prestiti fatti da utenti della biblioteca.
+        @param libro il libro in prestito
+        @param utente l'utente che ha il prestito attivo
+        @param data il giorno in cui è cominciato il prestito
+         */
 
-
-    public Prestito(Libro libro, Utente utente, Date data) {
+    /**
+     * add an observer to the list of observers
+     *
+     * @param o osservatore
+     */
+    @Override
+    public void addObserver(Observer o) {
 
     }
 
+    /**
+     * notify all observers
+     */
+    @Override
+    public void notifyObservers() {
+
+    }
+
+    /*
+    con observer continua a controllare che il prestito sia ancora in tempo per essere restituito
+    @param utente utente da verificare
+     */
+    public void controlloPrestito(Utente utente) {
+        notifyObservers();
+        if (LocalDate.now().toEpochDay() - finePrestito == 0) {
+            utente.sospendiUtente();
+        }
+
+    }
 }
